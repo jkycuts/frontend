518239265492bea3deaa7966caedc182ec41efad
jkycuts <jkayecutillas5@gmail.com>
2024-04-18 03:30:16 +0800

    Logout Fixes and User Logged

--- js/dashboard/dashboard.js
+++ js/dashboard/dashboard.js
@@ -18,7 +18,6 @@ btn_logout.onclick = async () => {
         /* Clear Token */
         localStorage.clear();
 
-        successNotification("Logout Successful!");
         /* Redirect Page */
         window.location.pathname = "/login.html";
 
@@ -29,3 +28,28 @@ btn_logout.onclick = async () => {
         errorNotification(json.message, 10);
     }
 };
+
+getLoggedUser();
+ async function getLoggedUser () {
+
+    /* Access User Profile API endpoint */
+    const response = await fetch(backendURL + "/api/profile/show", {
+        headers: {
+            Accept: "application/json",
+            Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+    });
+
+    // Get response if 200-299 status code
+    if (response.ok) {
+        const json = await response.json();
+
+        document.getElementById("user_logged").innerHTML = json.firstname + " " + json.lastname;
+    } 
+    // Get response if 400 or 500 status code
+    else {
+        const json = await response.json();
+
+        errorNotification(json.message, 10);
+    }
+}

f949d8ad58c5c952c4055522815fccc0d55389f8
jkycuts <jkayecutillas5@gmail.com>
2024-04-17 13:30:40 +0800

    Added Functionality in Registration Page

---
+++ js/auth/register.js
@@ -0,0 +1,50 @@
+import { url, successNotification, errorNotification } from '../utils/utils.js';
+
+/* Form Register */
+const form_register = document.getElementById("form_register");
+
+form_register.onsubmit = async (e) => {
+    e.preventDefault();
+
+    // Disable Button
+    document.querySelector("#form_register button").disabled = true;
+    document.querySelector("#form_register button").innerHTML = 
+    `<div class="spinner-border me-2" role="status"></div> 
+    <span>Loading...</span>` ;
+
+    // Get values of form (input, textarea, select) set it as form data
+    const formData = new FormData(form_register);
+
+    // Fetch User API user register endpoint
+    const response = await fetch(url + "/api/user", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+        },
+        body: formData,
+    });
+
+    // Get response if 200-299 status code
+    if (response.ok) {
+        const json = await response.json();
+        console.log(json);
+
+        document.querySelector(".alert-success").classList.remove("d-none");
+        document.querySelector(".alert-success").classList.add("d-block");
+
+        form_register.reset();
+
+        successNotification("Successfully Registered Account!", 5);
+
+    // Get response if 422 status code
+    } else if (response.status == 422) {
+        const json = await response.json();
+
+        errorNotification(json.message, 5);
+    }
+
+    // Enable Button
+    document.querySelector("#form_register button").disabled = false;
+    document.querySelector("#form_register button").innerHTML = `Create Account`;
+};
+
